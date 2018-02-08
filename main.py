a13d3aff84dcff9d6a5c43c621f1c7013838d265
Daniel Rozenzaft <drozenzaft@stuy.edu>
2018-02-08 11:38:47 -0500

    added extra tests for octave 1

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
-draw_line(0,0,200,200,screen,color)
+color = [0, 0, 255]
+for y in range(YRES/5):
+    '''x = (int)(y*(500/255.0))%255
+    color = [0,0,x]'''
+    draw_line(0,y*5,XRES,YRES,screen,color)
+    draw_line(y*5,0,XRES,YRES,screen,color)
 display(screen)
 #save_extension(screen, 'img.png')
